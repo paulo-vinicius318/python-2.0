4617869b8623020d095be67cb0dc02471a49af9f
Paulo <pvinicr@gmail.com>
2022-06-05 14:14:46 -0300

    duas alterações: criei uma linha 5 e uma assinatura

--- teste2.py
+++ teste2.py
@@ -2,8 +2,10 @@ print("opa, tudo bem?")
 print("Meu nome é Paulo Vinicius")
 print("Estou aprendendo a utilizar o github")
 print("isso é somente um teste!")
+print()
 print("05/06/2022")
 print("Curitiba/PR")
+print("Atte, Paulo Vinicius")
 
 valor=10
 if valor <= 10:
